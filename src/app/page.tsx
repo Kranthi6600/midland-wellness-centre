3a72c5d6ded96d19c71aa1ed113f815647de5135
kranthi <sriramkranthikumar7672@gmail.com>
2026-03-16 14:09:04 +0530

    Fix all import paths in main page: update from home1 to available components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 
 import { GoogleMap } from "@react-google-maps/api";
 import Layout from "../../components/layout/Layout";
-import About from "../../components/sections/home1/About";
-import About_Two from "../../components/sections/home1/About_Two";
-import Appointment from "../../components/sections/home1/Appointment";
-import Banner from "../../components/sections/home1/Banner";
-import Chooseus from "../../components/sections/home1/Chooseus";
-import Contact_Info from "../../components/sections/home1/Contact_Info";
-import News from "../../components/sections/home1/News";
-import Portfolio from "../../components/sections/home1/Portfolio";
-import Service from "../../components/sections/home1/Service";
-import Team from "../../components/sections/home1/Team";
-import Working from "../../components/sections/home1/Working";
-import GoogleMapSection from "../../components/sections/home1/GoogleMap";
+import About from "../../components/sections/home2/About";
+import About_Two from "../../components/sections/home2/About_Two";
+import Banner from "../../components/sections/home2/Banner";
+import Chooseus from "../../components/sections/home2/Chooseus";
+import Contact_Info from "../../components/sections/home3/Contact_Info";
+import News from "../../components/sections/home3/News";
+import Portfolio from "../../components/sections/home2/Portfolio";
+import Service from "../../components/sections/home2/Service";
+import Working from "../../components/sections/home2/Working";
 export default function Home() {
 
     return (
@@ -23,13 +20,10 @@ export default function Home() {
                 <About/>
                 <Service/>
                 <Chooseus/>
-                <Appointment/>
                 <Working/>
                 <About_Two/>
-                <Team/>
                 <Portfolio/>
                 <News/>
-                <GoogleMapSection/>
             </Layout>
         </div>
     )
