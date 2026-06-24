007234c100a4044bf69f3b6ea495dd4fc90b8718
kranthi kumar <kranthichintu5324@gmail.com>
2026-06-24 21:33:08 +0530

    Add Google Tag Manager (GTM-P3J676LC)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,11 @@ export default function RootLayout({
   return (
     <html lang="en-CA">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3J676LC');`,
+          }}
+        />
         <link rel="icon" href="/assets/images/header_logo.ico" sizes="any" />
         <link rel="canonical" href="https://midlandwellnesscentre.com" />
         <link rel="alternate" hrefLang="en" href="https://midlandwellnesscentre.com" />
@@ -118,6 +123,14 @@ export default function RootLayout({
         />
       </head>
       <body className={`${roboto.variable} ${caveat.variable} font-body`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3J676LC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <NextTopLoader
           color="#007acc"
           initialPosition={0.08}
